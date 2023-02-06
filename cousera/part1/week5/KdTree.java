934a03db6d5116268456558a35174ede527825af
Jang Sungil <jasuil@daum.net>
2023-02-06 19:28:24 +0900

    Add files via upload

---
+++ cousera/part1/week5/KdTree.java
@@ -0,0 +1,456 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class KdTree {
+
+    private BST2d points;            // construct an empty set of points
+    public KdTree() {
+        points = new BST2d();
+    }
+
+    public static void main(String[] args) {
+
+    }                 // unit testing of the methods (optional)
+
+    public boolean isEmpty() {
+        return points.size() > 0 ? false : true;
+    }                      // is the set empty?
+
+    public int size() {
+        return points.size();
+    }                        // number of points in the set
+
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        points.put(p);
+    }             // add the point to the set (if it is not already in the set)
+
+    public boolean contains(Point2D p) {
+        return points.contains(p);
+    }          // does the set contain point p?
+
+    public void draw() {
+        Iterator<Point2D> iterator = points.keys().iterator();
+        while (iterator.hasNext()) {
+            Point2D point = iterator.next();
+            StdDraw.point(point.x(), point.y());
+        }
+    }                        // draw all points to standard draw
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+        Queue<Point2D> residents = new Queue<>();
+        Iterator<Point2D> iterator = points.keys(rect.xmin(), rect.ymin(), rect.xmax(), rect.ymax()).iterator();
+        while (iterator.hasNext()) {
+            Point2D point = iterator.next();
+            if (rect.contains(point)) {
+                residents.enqueue(point);
+            }
+        }
+        return residents.isEmpty() ? new Stack<>() : residents;
+    }            // all points that are inside the rectangle (or on the boundary)
+
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        if (points.size() == 0) return null;
+        BST2d.Node bottom = points.nearest(p);
+        if (bottom == null) return null;
+        else {
+            double leftDiff = 1.0;
+            double rightDiff = 1.0;
+            if (bottom.left != null) leftDiff = bottom.left.key.distanceSquaredTo(p);
+            if (bottom.right != null) rightDiff = bottom.right.key.distanceSquaredTo(p);
+            double keyDiff = bottom.key.distanceSquaredTo(p);
+            if (Double.compare(rightDiff, leftDiff) > 0) {
+                if (Double.compare(keyDiff, leftDiff) > 0) {
+                    return bottom.left.key;
+                } else {
+                    return bottom.key;
+                }
+            } else {
+                if (Double.compare(keyDiff, rightDiff) > 0) {
+                    return bottom.right.key;
+                } else {
+                    return bottom.key;
+                }
+            }
+        }
+    }
+
+    private class BST2d {
+        private Node root;             // root of BST
+
+        /**
+         * Initializes an empty symbol table.
+         */
+        public BST2d() {
+        }
+
+        public int size() {
+            return size(root);
+        }
+
+        // return number of key-value pairs in BST rooted at x
+        private int size(Node x) {
+            if (x == null) return 0;
+            else return x.size;
+        }
+
+        public boolean contains(Point2D key) {
+            if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+            return get(key);
+        }
+
+        public boolean get(Point2D key) {
+            return get(root, key);
+        }
+
+        private boolean get(Node x, Point2D key) {
+            if (key == null) throw new IllegalArgumentException("calls get() with a null key");
+            if (x == null) return false;
+            int xCmp = Point2D.X_ORDER.compare(x.key, key);
+            int yCmp = Point2D.Y_ORDER.compare(x.key, key);
+
+            int cmp = -1;
+            if (x.isX) cmp = xCmp;
+            else cmp = yCmp;
+
+            if (xCmp == 0 && xCmp == yCmp) return true;
+            else if      (cmp > 0) return get(x.left, key);
+            else return get(x.right, key);
+
+        }
+
+        public void put(Point2D key) {
+            if (key == null) throw new IllegalArgumentException("calls put() with a null key");
+            root = put(root, key);
+            assert check();
+        }
+
+        private Node put(Node x, Point2D key) {
+            if (x == null) return new Node(key, 1);
+            int xCmp = Point2D.X_ORDER.compare(key, x.key);
+            int yCmp = Point2D.Y_ORDER.compare(key, x.key);
+
+            if (xCmp == 0 && xCmp == yCmp) return x;
+            else if (x.isX) {
+                if (xCmp < 0) {
+                    x.left = put(x.left,  key);
+                    x.left.isX = false;
+                }
+                else {
+                    x.right = put(x.right, key);
+                    x.right.isX = false;
+                }
+
+            } else {
+                if (yCmp < 0) {
+                    x.left  = put(x.left,  key);
+                    x.left.isX = true;
+                }
+                else {
+                    x.right = put(x.right, key);
+                    x.right.isX = true;
+                }
+
+            }
+            x.size = 1 + size(x.left) + size(x.right);
+
+            return x;
+        }
+
+        private boolean check() {
+            if (!isBST())            StdOut.println("Not in symmetric order");
+            if (!isSizeConsistent()) StdOut.println("Subtree counts not consistent");
+            if (!isRankConsistent()) StdOut.println("Ranks not consistent");
+            return isBST() && isSizeConsistent() && isRankConsistent();
+        }
+
+        // does this binary tree satisfy symmetric order?
+        // Note: this test also ensures that data structure is a binary tree since order is strict
+        private boolean isBST() {
+            return isBST(root, null, null);
+        }
+
+        // is the tree rooted at x a BST with all keys strictly between min and max
+        // (if min or max is null, treat as empty constraint)
+        // Credit: elegant solution due to Bob Dondero
+        private boolean isBST(Node x, Point2D min, Point2D max) {
+            if (x == null) return true;
+            if (min != null && x.key.compareTo(min) <= 0) return false;
+            if (max != null && x.key.compareTo(max) >= 0) return false;
+            return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+        }
+
+        private boolean isSizeConsistent() { return isSizeConsistent(root); }
+
+        private boolean isSizeConsistent(Node x) {
+            if (x == null) return true;
+            if (x.size != size(x.left) + size(x.right) + 1) return false;
+            return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+        }
+
+        // check that ranks are consistent
+        private boolean isRankConsistent() {
+            for (int i = 0; i < size(); i++)
+                if (i != rank(select(i))) return false;
+            for (Point2D key : keys())
+                if (key.compareTo(select(rank(key))) != 0) return false;
+            return true;
+        }
+
+        public Point2D select(int rank) {
+            if (rank < 0 || rank >= size()) {
+                throw new IllegalArgumentException("argument to select() is invalid: " + rank);
+            }
+            return select(root, rank);
+        }
+
+        // Return key in BST rooted at x of given rank.
+        // Precondition: rank is in legal range.
+        private Point2D select(Node x, int rank) {
+            if (x == null) return null;
+            int leftSize = size(x.left);
+            if      (leftSize > rank) return select(x.left,  rank);
+            else if (leftSize < rank) return select(x.right, rank - leftSize - 1);
+            else                      return x.key;
+        }
+
+        /**
+         * Return the number of keys in the symbol table strictly less than {@code key}.
+         *
+         * @param  key the key
+         * @return the number of keys in the symbol table strictly less than {@code key}
+         * @throws IllegalArgumentException if {@code key} is {@code null}
+         */
+        public int rank(Point2D key) {
+            if (key == null) throw new IllegalArgumentException("argument to rank() is null");
+            return rank(key, root);
+        }
+
+        // Number of keys in the subtree less than key.
+        private int rank(Point2D key, Node x) {
+            if (x == null) return 0;
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) return rank(key, x.left);
+            else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+            else              return size(x.left);
+        }
+
+        /**
+         * Returns all keys in the symbol table in ascending order,
+         * as an {@code Iterable}.
+         * To iterate over all of the keys in the symbol table named {@code st},
+         * use the foreach notation: {@code for (Key key : st.keys())}.
+         *
+         * @return all keys in the symbol table in ascending order
+         */
+        public Iterable<Point2D> keys() {
+            if (isEmpty()) return new Stack<Point2D>();
+            return keys(min(), max());
+        }
+
+        public Point2D min() {
+            if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
+            return min(root).key;
+        }
+
+        private Node min(Node x) {
+            if (x.left == null) return x;
+            else                return min(x.left);
+        }
+
+        /**
+         * Returns the largest key in the symbol table.
+         *
+         * @return the largest key in the symbol table
+         * @throws NoSuchElementException if the symbol table is empty
+         */
+        public Point2D max() {
+            if (isEmpty()) throw new NoSuchElementException("calls max() with empty symbol table");
+            return max(root).key;
+        }
+
+        private Node max(Node x) {
+            if (x.right == null) return x;
+            else                 return max(x.right);
+        }
+
+        public Iterable<Point2D> keys(Point2D lo, Point2D hi) {
+            if (lo == null) throw new IllegalArgumentException("first argument to keys() is null");
+            if (hi == null) throw new IllegalArgumentException("second argument to keys() is null");
+
+            Queue<Point2D> queue = new Queue<Point2D>();
+            keys(root, queue, lo, hi);
+            return queue;
+        }
+
+        private void keys(Node x, Queue<Point2D> queue, Point2D lo, Point2D hi) {
+            if (x == null) return;
+            int xComplo = Point2D.X_ORDER.compare(lo, x.key);
+            int yComplo = Point2D.Y_ORDER.compare(lo, x.key);
+            int xComphi = Point2D.X_ORDER.compare(hi, x.key);
+            int yComphi = Point2D.Y_ORDER.compare(hi, x.key);
+
+            int cmplo = -1; // lo.compareTo(x.key);
+            int cmphi = -1; // hi.compareTo(x.key);
+            if (x.isX) {
+                cmplo = xComplo;
+                cmphi = xComphi;
+            }
+            else {
+                cmplo = yComplo;
+                cmphi = yComphi;
+            }
+
+            if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+            if (cmplo < 0) keys(x.left, queue, lo, hi);
+            if (cmphi > 0) keys(x.right, queue, lo, hi);
+        }
+
+        public Iterable<Point2D> keys(double xMin, double yMin, double xMax, double yMax) {
+
+            Queue<Point2D> queue = new Queue<Point2D>();
+            keys(root, queue, xMin, yMin, xMax, yMax);
+            return queue;
+        }
+        private void keys(Node x, Queue<Point2D> queue, double xMin, double yMin, double xMax, double yMax) {
+            if (x == null) return;
+
+            int xComplo = x.key.x() > xMin ? 1 : x.key.x() < xMin ? -1 : 0;
+            int yComplo = x.key.y() > yMin ? 1 : x.key.y() < yMin ? -1 : 0;
+            int xComphi = x.key.x() > xMax ? 1 : x.key.x() < xMax ? -1 : 0;
+            int yComphi = x.key.y() > yMax ? 1 : x.key.y() < yMax ? -1 : 0;
+
+            int cmplo = -1; // lo.compareTo(x.key);
+            int cmphi = -1; // hi.compareTo(x.key);
+            if (x.isX) {
+                cmplo = xComplo;
+                cmphi = xComphi;
+            }
+            else {
+                cmplo = yComplo;
+                cmphi = yComphi;
+            }
+
+            if (cmplo >= 0 && cmphi <= 0) {
+                queue.enqueue(x.key);
+                keys(x.left, queue, xMin, yMin, xMax, yMax);
+                keys(x.right, queue, xMin, yMin, xMax, yMax);
+            } else if (cmphi > 0) {
+                queue.enqueue(x.key);
+                keys(x.left, queue, xMin, yMin, xMax, yMax);
+            } else if (cmplo < 0) {
+                queue.enqueue(x.key);
+                keys(x.right, queue, xMin, yMin, xMax, yMax);
+            }
+        }
+
+        /**
+         * Returns the number of keys in the symbol table in the given range.
+         *
+         * @param  lo minimum endpoint
+         * @param  hi maximum endpoint
+         * @return the number of keys in the symbol table between {@code lo}
+         *         (inclusive) and {@code hi} (inclusive)
+         * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+         *         is {@code null}
+         */
+        public int size(Point2D lo, Point2D hi) {
+            if (lo == null) throw new IllegalArgumentException("first argument to size() is null");
+            if (hi == null) throw new IllegalArgumentException("second argument to size() is null");
+
+            if (lo.compareTo(hi) > 0) return 0;
+            if (contains(hi)) return rank(hi) - rank(lo) + 1;
+            else              return rank(hi) - rank(lo);
+        }
+
+        public Node bottom(Point2D key) {
+            if (key == null) throw new IllegalArgumentException("argument to ceiling() is null");
+            if (isEmpty()) throw new NoSuchElementException("calls ceiling() with empty symbol table");
+            Node x = bottom(root, key);
+            if (x == null) return null;
+            else return x;
+        }
+
+        private Node bottom(Node x, Point2D key) {
+            if (x == null) return null;
+            int cmp = 0;
+
+            if (x.isX) cmp = Point2D.X_ORDER.compare(x.key, key);
+            else cmp = Point2D.Y_ORDER.compare(x.key, key);
+
+            if (cmp > 0) {
+                Node t = bottom(x.left, key);
+                if (t != null) return t;
+                else return x;
+            } else if (cmp < 0) {
+                Node t = bottom(x.right, key);
+                if (t != null) return t;
+                else return x;
+            } else {
+                return x;
+            }
+
+        }
+
+        public Node nearest(Point2D key) {
+            if (key == null) throw new IllegalArgumentException("argument to ceiling() is null");
+            if (isEmpty()) throw new NoSuchElementException("calls ceiling() with empty symbol table");
+            double rootDist = key.distanceSquaredTo(root.key);
+            Node x = nearest(root, key, rootDist);
+            if (x == null) return null;
+            else return x;
+        }
+
+        private Node nearest(Node x, Point2D key, double rootDist) {
+            if (x == null) return null;
+            int cmp = 0;
+
+            if (x.isX) cmp = Point2D.X_ORDER.compare(x.key, key);
+            else cmp = Point2D.Y_ORDER.compare(x.key, key);
+
+            if (cmp > 0) {
+                Node t = nearest(x.left, key, rootDist);
+                if (t != null) {
+                    double tDist = t.key.distanceSquaredTo(key);
+                    rootDist = x.key.distanceSquaredTo(key);
+                    if (Double.compare(rootDist, tDist) > 0) return t;
+                }
+                else return x;
+            } else if (cmp < 0) {
+                Node t = nearest(x.right, key, rootDist);
+                if (t != null) {
+                    double tDist = t.key.distanceSquaredTo(key);
+                    rootDist = x.key.distanceSquaredTo(key);
+                    if (Double.compare(rootDist, tDist) > 0) return t;
+                }
+                else return x;
+            } else {
+                return x;
+            }
+
+            return x;
+        }
+
+        private class Node {
+            private Point2D key;           // sorted by key
+            private boolean isX;
+            private Node left, right;  // left and right subtrees
+            private int size;          // number of nodes in subtree
+
+            public Node(Point2D key, int size) {
+                this.key = key;
+                this.size = size;
+                isX = true;
+            }
+        }
+
+    }
+}
---
+++ cousera/part1/week5/Test.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.Iterator;
+
+public class Test {
+
+    public static void main(String[] args) {
+        KdTree kdTree = new KdTree();
+        PointSET pointSET = new PointSET();
+        In in = new In(args[0]);
+        int n = in.readInt();
+        for (int i = 0; i < n; i++) {
+            Point2D input = new Point2D(in.readDouble(), in.readDouble());
+            kdTree.insert(input);
+            pointSET.insert(input);
+        }
+
+        RectHV rectHV = new RectHV(0.25, 0.0,1.0, 0.75);
+        Iterator<Point2D> iterator = kdTree.range(rectHV).iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+     //   Point2D nearest = kdTree.nearest(new Point2D(0.476, 0.537));
+     //   System.out.println(nearest);
+       // nearest = pointSET.nearest(new Point2D(0.75, 0.5));
+       // System.out.println(nearest);
+    }
+}
